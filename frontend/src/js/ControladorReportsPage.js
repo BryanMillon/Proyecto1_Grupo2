f9b6f0b04e8ebe52721e4bfd494d45648a2a2c18
jcazang1 <jcazangas@ucenfotec.ac.cr>
2025-04-19 18:47:57 -0600

    Actualizacion denuncias

--- frontend/src/js/ControladorReportsPage.js
+++ frontend/src/js/ControladorReportsPage.js
@@ -57,3 +57,41 @@ btnCancel.addEventListener("click", function () {
     textPlace.value = "";
     textDescription.value = "";
 });
+
+//Rutina para eliminar del header las acciones del Admin
+
+const tipoUsuario = localStorage.getItem("rolLogIn"); 
+const adminItemHeader = document.getElementById("adminNavItemHeader");
+const adminItemFooter = document.getElementById("adminNavItemFooter");
+const crearAviso = document.getElementById("crearAviso");
+const crearNoticia = document.getElementById("crearNoticia");
+const botonPublicar = document.getElementById("btnBotonPublicar");
+
+
+console.log(tipoUsuario)
+
+// Ocultar la opción de ADMINISTRADOR si no es administrador
+if (tipoUsuario !== "administrador") {
+    if (adminItemHeader) {
+        adminItemHeader.style.display = "none";
+    }
+}
+
+// Ocultar la opción de ADMINISTRADOR si no es administrador
+if (tipoUsuario !== "administrador") {
+    if (adminItemFooter) {
+        adminItemFooter.style.display = "none";
+    }
+}
+
+if (tipoUsuario == "vecino") {
+    if (crearAviso) {
+        crearAviso.style.display = "none";
+    }
+}
+
+if (tipoUsuario == "vecino") {
+    if (crearNoticia) {
+        crearNoticia.style.display = "none";
+    }
+}
